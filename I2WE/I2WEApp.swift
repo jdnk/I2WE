7b7af589056db7433e9ac2c714d9782bb6e9e3a4
Jaden Kim <jadenikim@gmail.com>
2020-10-12 00:16:57 -0700

    Initial Commit

---
+++ I2WE/I2WEApp.swift
@@ -0,0 +1,17 @@
+//
+//  I2WEApp.swift
+//  I2WE
+//
+//  Created by Jaden Kim on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct I2WEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
